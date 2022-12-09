db5b2dcaaa572672b753600cae9f581347b681f3
Brenton <brenton@breakdesign.com.au>
2022-12-09 11:43:48 +1100

    updated based on feedback

--- app.py
+++ app.py
@@ -1,17 +1,40 @@
 from models import db, Portfolio, app, get_datetime_from_date_string
-from flask import (render_template, request, redirect, url_for, flash)
+from flask import (render_template, request, redirect, url_for)
 
 
-#home route
+# home route
 @app.route('/')
 def home():
     portfolio_items = Portfolio.query.all()
     return render_template('index.html', portfolio_items=portfolio_items)
 
-#create route
+
+# about route
+@app.route('/about')
+def about():
+    portfolio_items = Portfolio.query.all()
+    return render_template('about.html', portfolio_items=portfolio_items)
+
+
+# skills route
+@app.route('/skills')
+def skills():
+    portfolio_items = Portfolio.query.all()
+    return render_template('skills.html', portfolio_items=portfolio_items)
+
+
+# contact route
+@app.route('/contact')
+def contact():
+    portfolio_items = Portfolio.query.all()
+    return render_template('contact.html', portfolio_items=portfolio_items)
+
+
+# create route
 @app.route('/new', methods=['GET', 'POST'])
 def create():
     print(request.form)
+    all_portfolio_items = Portfolio.query.all()
     if request.form:
             dateToUse = get_datetime_from_date_string(
                 request.form['date'] + "-01")
@@ -19,18 +42,22 @@ def create():
             db.session.add(portfolio)
             db.session.commit()
             return redirect(url_for('home'))
-    return render_template('projectform.html')
+    return render_template('projectform.html', all_portfolio_items=all_portfolio_items)
 
-#detail route
+
+# detail route
 @app.route('/<id>')
 def detail(id):
     portfolio_item = Portfolio.query.get(id)
-    return render_template('detail.html', portfolio_item=portfolio_item)
+    all_portfolio_items = Portfolio.query.all()
+    return render_template('detail.html', portfolio_item=portfolio_item, all_portfolio_items=all_portfolio_items)
 
-#edit route
+
+# edit route
 @app.route('/<id>/edit', methods=['GET', 'POST'])
 def edit(id):
     portfolio_item = Portfolio.query.get(id)
+    all_portfolio_items = Portfolio.query.all()
     if request.form:
         dateToUse = get_datetime_from_date_string(
             request.form['date'] + "-01")
@@ -41,9 +68,10 @@ def edit(id):
         portfolio_item.githuburl = request.form['github']
         db.session.commit()
         return redirect(url_for('home'))
-    return render_template('edit.html', portfolio_item=portfolio_item)
+    return render_template('edit.html', portfolio_item=portfolio_item, all_portfolio_items=all_portfolio_items)
 
-#delete route
+
+# delete route
 @app.route('/<id>/delete', methods=['GET', 'POST'])
 def delete(id):
     portfolio = Portfolio.query.get(id)
@@ -51,9 +79,11 @@ def delete(id):
     db.session.commit()
     return redirect(url_for('home'))
 
-#dunder main
+
+# dunder main
 if __name__ == '__main__':
     #run the app
     with app.app_context():
         db.create_all()
     app.run(debug=True, port=8000, host='127.0.0.1')
+
